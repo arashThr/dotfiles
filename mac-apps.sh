1073827f46f1bc9c4858f67102e408d2dbb1b935
Arash Taher <arashThr@outlook.com>
2024-07-09 17:08:34 +0200

    Update mac apps

--- mac-apps.sh
+++ mac-apps.sh
@@ -3,7 +3,7 @@
 # Install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install zsh git neovim gh tmux
+brew install zsh git neovim gh tmux fzf emacs jq
 
 # Oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
