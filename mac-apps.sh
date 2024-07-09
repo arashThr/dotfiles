9f92471e670882e86ebb06c1eb7fee6f3b5f1742
Arash Taher <arashThr@outlook.com>
2024-07-09 16:17:59 +0200

    Separate mac and linux setup

---
+++ mac-apps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install zsh git neovim gh tmux
+
+# Oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
