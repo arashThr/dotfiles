199b76b4d6aca893f1a0cbe5d7e51393abbba9e2
Arash Taher <arashThr@outlook.com>
2024-07-16 14:00:49 +0200

    Update mac apps

--- mac-apps.sh
+++ mac-apps.sh
@@ -3,8 +3,13 @@
 # Install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install zsh git neovim gh tmux fzf emacs jq
+brew install zsh git neovim gh tmux fzf emacs jq docker nvm libpq
 
 # Oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
+export PATH="/opt/homebrew/opt/libpq/bin:$PATH"
